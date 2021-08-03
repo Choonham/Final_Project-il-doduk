91fe64cabc6975032fd30eda796ac0a048e99db8
SummerH <godnjs729417@gmail.com>
2021-08-03 16:15:39 +0900

    [UPDATE] (AUCTION) 0803 /onAuctionList 수정 - AuctionController, onAuctionList.html, PageResultsDTO 수정 onAuctionList.html에 JQuery를 통해 경매 진행 중인 목록과 경매 완료 미매칭 목록을 출력할 수 있게 변환, PageResultsDTO.java에 전체 엘리먼트 확인 변수 추가

--- src/main/java/com/finalproject/ildoduk/repository/auction/AuctionListRepository.java
+++ src/main/java/com/finalproject/ildoduk/repository/auction/AuctionListRepository.java
@@ -48,7 +48,7 @@ public interface AuctionListRepository extends JpaRepository<AuctionList, Long>,
 
     //====================================== Blog =====================================//
 
-    //헬퍼 기준 일 수행 완료 된 갑 불러오기 state=3, auction-bidding <List>
+    //헬퍼 기준 일 수행 완료 된 값 불러오기 state=3, auction-bidding <List>
     @Query(value = "SELECT a,b FROM AuctionList a, BiddingList b WHERE a.state=3 and b.helper.id=:helper and a.aucSeq=b.aucSeq.aucSeq and b.chosen=1")
     List<Object[]> getAllWith4ForHelper(String helper);
 }
